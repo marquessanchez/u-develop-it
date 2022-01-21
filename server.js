f55d3e20b307329a1cce5056c69496af93f8ea74
mrican217@gmail.com <mrican217@gmail.com>
2022-01-20 22:36:56 -0500

    setting up node.js application

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'marques',
+    // Your MySQL password
+    password: 'password',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
