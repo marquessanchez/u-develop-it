a0d00875717555d48beb8dbce88867f5c42c2df4
mrican217@gmail.com <mrican217@gmail.com>
2022-01-29 09:44:13 -0500

    created parties table

--- server.js
+++ server.js
@@ -22,60 +22,6 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
-// // Get all candidates
-// app.get('/api/candidates', (req, res) => {
-//   const sql = `SELECT * FROM candidates`;
-
-//   db.query(sql, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: rows
-//     });
-//   });
-// });;
-
-//   // GET a single candidate
-//   app.get ('/api/candidate/:id', (req, res) => {
-//     db.query(sql, (err, row) => {
-//       const sql = `SELECT * FROM candidates`
-//       if (err) {
-//         console.log(err);
-//       }
-//       console.log(row);
-//       });
-// });
-
-// // Delete a candidate
-// app.delete('/api/candidate/:id', (req, res) => {
-//   const sql = `DELETE FROM candidates WHERE id = ?`;
-//   const params = [req.params.id];
-
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.statusMessage(400).json({ error: res.message });
-//     } else if (!result.affectedRows) {
-//       res.json({
-//         message: 'Candidate not found'
-//       });
-//     } else {
-//       res.json({
-//         message: 'deleted',
-//         changes: result.affectedRows,
-//         id: req.params.id
-//       });
-//     }
-//   });
-// });
-
-// // Default response for any other request (Not Found)
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
-
 // // Start server after DB connection
 // db.connect(err => {
 //   // if (err) throw err;
