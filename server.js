218bd5f19b5d3e9a4e778bac28e4167222d7e9ea
mrican217@gmail.com <mrican217@gmail.com>
2022-01-29 02:22:43 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 // const db = require('.db/db')
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -80,9 +81,84 @@ const db = mysql.createConnection(
 //   // if (err) throw err;
 //   console.log('Database connected.');
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+
+// Get all candidates 
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({error: err.message});
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
 });
+
+
+// Delete a candidate
+app.delete('api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected')
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.mesaage });
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+
+// Get a single candidate
+app.get('api/candidates/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+db.query(sql, params, (err, rows) => {
+  if (err) {
+    res.status(400).json({ error : err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
 
 // Default response for any other request (Not Found)
 // Catchall route, this route can override all other routes
